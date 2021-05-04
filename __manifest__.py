9cc6e89fd6f49065af3038ac3fe1e8564ca1c508
Golam Mostafa <sujon1mostafa@gmail.com>
2021-05-04 17:36:18 +0600

    basic odoo structure created

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name' : 'University Management (manifest,name)',
+    'version' : '1.0',
+    'sequence' : 5,
+    'description' : 'University Management Description (manifest)',
+    'depends' : ['base'],
+    'data' : [
+
+    ],
+    'installable' : True,
+    'application' : True,
+    'auto_install' : False,
+}
