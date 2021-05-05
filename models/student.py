6576c80c8ceac244fb354e8e10d68caefd1b6f4f
Golam Mostafa <sujon1mostafa@gmail.com>
2021-05-05 16:17:05 +0600

    student 1.0

---
+++ models/student.py
@@ -0,0 +1,12 @@
+from odoo import fields, models, api, _
+
+class UniStudent(models.Model):
+    _name = 'uni.student'
+    _description = 'Student Information'
+
+    name = fields.Char(string='Student Name')
+    stu_id = fields.Char(string='student ID')
+    department = fields.Char(string='Department')
+    credi_earn = fields.Integer(string='Credit Earned')
+    course_taken = fields.Char(string='Ongoing Courses')
+    photo = fields.Binary(string="Photo", attachment=True)
