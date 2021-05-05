8705d43397e10dfaf2a5e2e9a40166d08f9f7f1c
Golam Mostafa <sujon1mostafa@gmail.com>
2021-05-05 14:56:13 +0600

    faculty 1.1

--- models/faculty.py
+++ models/faculty.py
@@ -2,9 +2,14 @@ from odoo import api, models, fields, _
 
 class UniFaculty(models.Model):
     _name = 'uni.faculty'
-    _description = 'Faculty Information'
+    _description = 'Faculty Information c'
 
     name = fields.Char(string='Faculty Name')
     dept_name = fields.Char(string='Department')
     salary = fields.Float(String='Salary')
     course_id = fields.Char(string='Courses')
+    designation = fields.Selection([('professor', 'Professor'), 
+                                ('lecturer', 'Lecturer'),
+                                ('adjunct_faculty', 'Adjunct Faculty')], 
+                                default='lecturer')
+    photo = fields.Binary(string="Photo", attachment=True)
