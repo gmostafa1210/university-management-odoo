7ff90958a47aca91f7ccc89a1365ad4779ad0dae
Golam Mostafa <sujon1mostafa@gmail.com>
2021-05-07 02:12:16 +0600

    _rec_name added to course

--- models/course.py
+++ models/course.py
@@ -3,8 +3,9 @@ from odoo import models, fields
 class UniCourse(models.Model):
     _name = 'uni.course'
     _description = 'Course Information'
+    _rec_name = 'course_name'
 
-    name = fields.Char(string='Course Name')
+    course_name = fields.Char(string='Course Name')
     course_code = fields.Char(string='Course Code')
     course_description = fields.Text(string='Course Description')
     credit = fields.Integer(string='Credit')
