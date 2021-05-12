63bc47edafb9b5c0bfb759acf1ee5d29758a3a96
Golam Mostafa <sujon1mostafa@gmail.com>
2021-05-12 16:41:21 +0600

    api customized in course

--- models/course.py
+++ models/course.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api,  _
+from odoo.exceptions import UserError 
 
 class UniCourse(models.Model):
     _name = 'uni.course'
@@ -8,4 +9,27 @@ class UniCourse(models.Model):
     course_name = fields.Char(string='Course Name')
     course_code = fields.Char(string='Course Code')
     course_description = fields.Text(string='Course Description')
-    credit = fields.Integer(string='Credit')
+    credit_hour = fields.Integer(string='Credit')
+    have_lab = fields.Char(string='Lab')
+
+    @api.model
+    def create(self, values):
+        if values['credit_hour'] != 3 and values['credit_hour'] != 4:
+            raise UserError(_('Invalid input. Please input 3 or 4.'))
+        else:
+            return super(UniCourse, self).create(values)
+    
+    def write(self, values):
+        if values['credit_hour']:
+            if values['credit_hour'] != 3 and values['credit_hour'] != 4:
+                raise UserError(_('Invalid input. Please input 3 or 4.'))
+            else:
+                return super(UniCourse, self).write(values)
+
+    @api.onchange('credit_hour')
+    def lab_assign(self):
+        if self.credit_hour:
+            if self.credit_hour == 3:
+                self.have_lab = 'No'
+            elif self.credit_hour == 4:
+                self.have_lab = 'Yes'
