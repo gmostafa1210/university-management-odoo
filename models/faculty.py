6a671c4b9efb8c103b4b58e4654d0a02984bc274
Golam Mostafa <sujon1mostafa@gmail.com>
2021-05-18 12:28:33 +0600

    compute field added to faculty

--- models/faculty.py
+++ models/faculty.py
@@ -4,7 +4,9 @@ class UniFaculty(models.Model):
     _name = 'uni.faculty'
     _description = 'Faculty Information c'
 
-    name = fields.Char(string='Faculty Name')
+    first_name = fields.Char(string='First Name')
+    last_name = fields.Char(string='Last Name')
+    name = fields.Char(string='Full Name', compute='_get_full_name')
     department_id = fields.Many2one('uni.department', string='Department')
     salary = fields.Float(String='Salary')
     course_id = fields.Char(string='Courses')
@@ -13,3 +15,16 @@ class UniFaculty(models.Model):
                                 ('adjunct_faculty', 'Adjunct Faculty')], 
                                 default='lecturer')
     photo = fields.Binary(string="Photo", attachment=True)
+
+    def _get_full_name(self):
+        first_name = ''
+        last_name = ''
+        for item in self:
+            if item.first_name:
+                first_name = item.first_name
+            if item.last_name:
+                last_name = item.last_name
+
+            item.name = first_name + ' ' + last_name
+
+    
