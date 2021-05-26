5e0e2faab60b56d4349e7047fa7911baba14c7b9
Golam Mostafa <sujon1mostafa@gmail.com>
2021-05-26 18:00:06 +0600

    orm added in student

--- models/student.py
+++ models/student.py
@@ -1,4 +1,5 @@
 from odoo import fields, models, api, _
+from odoo.exceptions import UserError
 
 class UniStudent(models.Model):
     _name = 'uni.student'
@@ -10,3 +11,11 @@ class UniStudent(models.Model):
     credit_earn = fields.Integer(string='Credit Earned')
     course_ids = fields.Many2many('uni.course', string='Courses')
     photo = fields.Binary(string="Photo", attachment=True)
+
+    def check_orm(self):
+        #ORM
+        all_data = self.env['uni.course'].search([('credit_hour','=',3)])
+        for data in all_data:
+            print(data.course_name)
+
+        raise UserError(_(all_data))
